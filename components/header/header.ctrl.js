fa0b8d90775102ec6016ca0a1bfbef9891687c8a
camel <camel>
2015-11-16 19:42:04 +0100

    removing old files

--- components/header/header.ctrl.js
+++
@@ -1,5 +0,0 @@
-app.controller('HeaderController', function ($scope) {
-        $scope.upgradeAccount = 'Upgrade account';
-        $scope.manageAccount = 'Manage account';
-        $scope.logOut = 'Log out';
-    });
