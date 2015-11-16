59990f7a1c31f79506ac7787c20e1b6167a371eb
camel <camel>
2015-11-16 19:03:44 +0100

    changes

---
+++ components/header/header.ctrl.js
@@ -0,0 +1,5 @@
+app.controller('HeaderController', function ($scope) {
+        $scope.upgradeAccount = 'Upgrade account';
+        $scope.manageAccount = 'Manage account';
+        $scope.logOut = 'Log out';
+    });
---
+++ components/home/home.directive.js
@@ -0,0 +1,3 @@
+/**
+ * Created by camel on 2015-11-11.
+ */
